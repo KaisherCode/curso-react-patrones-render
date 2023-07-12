855db98367dc82e2a5018a5cdf416801ecd052eb
Wilson Dev <wilson.hch@hynkor.com>
2023-07-12 14:15:53 -0500

    Creando tu primer HOC

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App/index.js';
+// import App from './App/index.js';
 import './index.css';
 
+function App(props) {
+  return (
+    <h1>¡{props.saludo}, {props.nombre}!</h1>
+  )
+}
+
+function withCualquierCosa(WrappedComponent) {
+  return function ComponenteDeVerdad(props) {
+    return (
+      <>
+        <WrappedComponent {...props} />
+        <p>Estamos acompañando al WrappedComponent</p>
+      </>
+    )
+  }
+}
+
+const AppWithCualquierCosa = withCualquierCosa(App)
+
 ReactDOM.render(
-  <App />,
+  <AppWithCualquierCosa saludo="Hey" nombre="Nath" />,
+  // <App saludo="Buenas" nombre="Nath"/>,
   document.getElementById('root')
 );
